2df529ebc1927b0a09684ce236726f30ca1b4d15
Aljosha Armitano <aarmitano@advanced-schema.com>
2020-07-30 19:45:05 +0200

    Add result for roller_coaster test files

--- test/run.sh
+++ test/run.sh
@@ -4,12 +4,12 @@ testPathAndResult="
 1.simple.test;7
 2.1000_groups.test;3935
 3.few_groups.test;15
-4.once_a_day.test;15000
 5.high_earnings.test;4999975000
 6.large_dataset.test;89744892565569
-roller_coaster.hard;1
-roller_coaster.harder;1
+roller_coaster.hard;8974489271113753
+roller_coaster.harder;89744892714152289
 "
+
 for line in ${testPathAndResult}
 do
    filePath=`echo $line | awk -F';' '{print $1}'`
